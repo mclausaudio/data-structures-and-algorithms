305e32960b0ea78bd826dca596cfb7838cd5c7e0
Michael_Claus <mclausaudio@gmail.com>
2022-09-12 16:22:29 -0700

    typo

--- leet-code/2325-Decode-the-Message/answer_refactor.js
+++ leet-code/2325-Decode-the-Message/answer_refactor.js
@@ -1,6 +1,6 @@
 var decodeMessage = function (key, message) {
   const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  const hash = new Map(); ``
+  const hash = new Map();
   key = [... new Set(key.split(" ").join(""))]
   let answer = "";
 
